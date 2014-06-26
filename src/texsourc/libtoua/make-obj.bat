d8792736a3bc5aef18cedd4a78851e46246bbf0a
maqiyuan <maqiyuan@users.sourceforge.jp>
2014-06-26 10:13:51 +0800

    check_existence().

--- src/texsourc/libtoua/make-obj.bat
+++
@@ -1,5 +0,0 @@
-cl -nologo -c -I"ptexenc" -I"../kpathsea" ptexenc/kanjicnv.c
-cl -nologo -c -I"ptexenc" -I"../kpathsea" ptexenc/ptexenc.c
-cl -nologo -c -I"ptexenc" -I"../kpathsea" ptexenc/unicode.c
-cl -nologo -c -I"ptexenc" -I"../kpathsea" ptexenc/unicode-jp.c
-
